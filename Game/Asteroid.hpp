19b04e615ee6b22454d956c075f06fab333bb0f9
JackBrennan9212 <32307467+JackBrennan9212@users.noreply.github.com>
2018-04-21 21:18:33 -0400

    Update Asteroid.hpp

--- Game/Asteroid.hpp
+++ Game/Asteroid.hpp
@@ -10,5 +10,22 @@
 #define Asteroid_hpp
 
 #include <stdio.h>
+class Asteroid : public MovingGameEntity
+{
+public:
+	Asteroid(SDL_Renderer* renderer);
+	~Asteroid();
+
+	bool checkCollisions(SDL_Rect* mainCollider, SDL_Rect* leftCollider, SDL_Rect* rightCollider);
+	void Update();
+	void Render();
+	void getRandomSprite();
+
+private:
+	SDL_Texture* texture;
+	SDL_Rect collisionRect;
+	SDL_Rect Clip;
+	std::string path;
+};
 
 #endif /* Asteroid_hpp */
